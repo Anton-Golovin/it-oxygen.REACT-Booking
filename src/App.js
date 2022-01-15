b6127ba237ddd5ec18078b57c2cc05e68a259dd0
Anton Holovin <anton.holovin@forforce.com>
2022-01-15 17:29:19 +0200

    CRA + DEV TOOLS

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header linkText={"Learn React"} />
+    </div>
+  );
+}
+
+export default App;
